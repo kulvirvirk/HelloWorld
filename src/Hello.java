0beb970e2ba7a4214aa2d31405b6b85ae00b062b
Kulvir Virk <kvirkdev@gmail.com>
2018-09-06 12:20:15 -0700

    added code for variables

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,13 @@
 public class Hello {
     public static void main(String [] args){
-        System.out.println("Hello world !");
-        System.out.println("Hello world1 !");
-        System.out.println("Hello world2 !");
+        int myFirstNumber = 5+5;
+        System.out.println("First Number: " + myFirstNumber);
+        System.out.println();
+
+        int myTotal = myFirstNumber + 10;
+        System.out.println("Total is:" + myTotal);
+
+        int var1 = 1000 - myTotal;
+        System.out.println(var1);
     }
 }
